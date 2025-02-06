5da89a26c09c2bb660fda7d567f732e202e2447b
Acer <Acer@DESKTOP-LHN9MF2.lan>
2025-02-06 20:11:03 +0530

    first commit

---
+++ Practice_Jenkins/src/test/java/practice/BuildSchedule.java
@@ -0,0 +1,13 @@
+package practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class BuildSchedule 
+{
+	@Test
+	public void simpletest()
+	{
+		Reporter.log("BuildSchedule class executed",true);
+	}
+}
